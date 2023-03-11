f46bd64c4e210c790dd4a92a996523de8099376e
amanleykj <118389631+amanleykj@users.noreply.github.com>
2023-03-11 08:56:00 -0800

    first commit is here now.

---
+++ week1/destructuring.js
@@ -0,0 +1,23 @@
+// #1
+
+// Tesla
+// Mercedes
+
+// #2
+
+// This would produce an error, as a console.log(name) is trying to access a key in an object. You would need to write it as console.log(employee.name) to get a result. For this reason, the console.log(otherName) would also fail.
+
+// #3
+
+// This would definitely print the string '12345'. It shouldn't print anything for the next line, however as password is not a key in the object person. 
+
+// #4
+
+// This would be false and true.
+
+// #5
+
+// value
+// [ 1, 5, 1, 8, 3, 3 ]
+// 1
+// 5
